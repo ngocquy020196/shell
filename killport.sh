73ca6cef9c4287954dd8370fb2af048f2f80de9f
Nguyễn Ngọc Quý <ngocquy020196@gmail.com>
2020-05-06 15:02:24 +0700

    Update killport.sh

--- killport.sh
+++ killport.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$(id -u)" != "0" ];then
+    echo "$(tput setaf 1)This script must be run as root";
+    exit 1;
+fi
+
 rm -rf /bin/killport
 
 tee -a /bin/killport > /dev/null <<EOT
@@ -7,10 +12,12 @@ tee -a /bin/killport > /dev/null <<EOT
 PORT=\$1;
 
 if [ ! -n "\$1" ]; then
-  echo "PORT NOT FOUND"
+  echo "\$(tput setaf 1)Port not found";
 else
-  fuser -n tcp -k \$PORT 
+  fuser -n tcp -k \$PORT;
 fi
 EOT
 
-chmod -R 755 /bin/killport
+chmod -R 755 /bin/killport;
+
+echo "$(tput setaf 2)Install success";
